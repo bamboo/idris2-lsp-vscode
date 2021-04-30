7e89978c3fae85e884084e0e48b29e871925fddb
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2021-04-30 11:02:28 -0300

    Simplify server command

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -14,15 +14,11 @@ let client: LanguageClient;
 export function activate(context: ExtensionContext) {
 	const extensionConfig = workspace.getConfiguration("idris2-lsp");
 	const command: string = extensionConfig.get("path") || "";
-	const serverOptions: ServerOptions = {
-		run: { command: command, },
-		debug: { command: command, }
+	const serverOptions: ServerOptions = { 
+		command: command,
 	};
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'idris' }],
-		synchronize: {
-			// fileEvents: workspace.createFileSystemWatcher('**/.idr')
-		},
 	};
 	client = new LanguageClient(
 		'idris2-lsp',
